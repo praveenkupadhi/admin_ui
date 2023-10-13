b76508dad70a4c37f2a7fa90168a80fcc9567225
Praveen Kumar Padhi <93372387+praveenkupadhi@users.noreply.github.com>
2023-10-14 01:52:22 +0530

    removed console logs

--- src/Components/AdminUI/AdminHome.jsx
+++ src/Components/AdminUI/AdminHome.jsx
@@ -1,37 +1,35 @@
-import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-	fetchDataFailure,
-	fetchDataRequest,
-	fetchDataSuccess,
-} from "../Redux/actions";
-import { DataRender } from "./DataRender";
+  fetchDataFailure,
+  fetchDataRequest,
+  fetchDataSuccess
+} from '../Redux/actions';
+import { DataRender } from './DataRender';
 
 export const AdminHome = () => {
-	const data = useSelector((store) => store.data);
-	const loading = useSelector((store) => store.loading);
-	const dispatch = useDispatch();
+  const data = useSelector((store) => store.data);
+  const loading = useSelector((store) => store.loading);
+  const dispatch = useDispatch();
 
-	// console.log(data);
+  useEffect(() => {
+    dispatch(fetchDataRequest());
+    if (data.length === 0) {
+      axios
+        .get(
+          'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json'
+        )
+        .then((res) => {
+          dispatch(fetchDataSuccess(res.data));
+        })
+        .catch((error) => {
+          dispatch(fetchDataFailure(error));
+        });
+    } else {
+      dispatch(fetchDataSuccess(data));
+    }
+  }, [dispatch]);
 
-	useEffect(() => {
-		dispatch(fetchDataRequest());
-		if (data.length === 0) {
-			axios
-				.get(
-					"https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
-				)
-				.then((res) => {
-					dispatch(fetchDataSuccess(res.data));
-				})
-				.catch((error) => {
-					dispatch(fetchDataFailure(error));
-				});
-		} else {
-			dispatch(fetchDataSuccess(data));
-		}
-	}, [dispatch]);
-
-	return loading ? <h2>Loading...</h2> : <DataRender />;
+  return loading ? <h2>Loading...</h2> : <DataRender />;
 };
