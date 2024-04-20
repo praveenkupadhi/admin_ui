fa4f0233d115876f9b207a83331834078ec501e4
praveenkupadhi <praveen7438@gmail.com>
2024-04-21 01:18:27 +0530

    fix: format

--- src/Components/Redux/store.js
+++ src/Components/Redux/store.js
@@ -1,7 +1,7 @@
+import { reducer } from './reducer';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { reducer } from './reducer';
 
 export const store = createStore(
   reducer,
