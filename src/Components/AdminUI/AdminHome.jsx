7371debbaa239d4ca7417d9346702a4a2985f833
Praveen Kumar Padhi <praveen7438@gmail.com>
2024-04-21 01:22:34 +0530

    fix: format

--- src/Components/AdminUI/AdminHome.jsx
+++ src/Components/AdminUI/AdminHome.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
 import { useEffect } from 'react';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { DataRender } from './DataRender';
 import {
