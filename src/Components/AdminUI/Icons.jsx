db8c228c04c68ca827a310fd72f5cb4ed673bc73
praveenkupadhi <praveen7438@gmail.com>
2024-05-05 22:50:58 +0530

    refactor: export

--- src/Components/AdminUI/Icons.jsx
+++ src/Components/AdminUI/Icons.jsx
@@ -2,14 +2,16 @@ import { FiEdit } from 'react-icons/fi';
 import { MdDeleteOutline } from 'react-icons/md';
 import { Component } from 'react';
 
-export class Edit extends Component {
+class Edit extends Component {
   render() {
     return <FiEdit />;
   }
 }
 
-export class Delete extends Component {
+class Delete extends Component {
   render() {
     return <MdDeleteOutline />;
   }
 }
+
+export { Edit, Delete };
